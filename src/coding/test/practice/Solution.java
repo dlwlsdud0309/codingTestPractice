964a97739aed262dbb9990528ba179afeb129903
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-04 02:43:12 +0900

    나머지 구하기

--- src/coding/test/practice/Solution.java
+++ src/coding/test/practice/Solution.java
@@ -5,9 +5,17 @@ public class Solution {
 		int answer=num1*num2;
 		return answer;
 	}
+	public int divide(int num1, int num2) {
+		int answer=num1%num2;
+		return answer;
+	}
+
 	public static void main(String[] args) {
 		Solution sol=new Solution();
-		int answer=sol.soulution(3, 7);
-		System.out.println("정답 : "+answer);
+		int answer1=sol.soulution(3, 7);
+		System.out.println("정답1 : "+answer1);
+		
+		int answer2=sol.divide(3, 2);
+		System.out.println("정답2 : "+answer2);
 	}
 }
