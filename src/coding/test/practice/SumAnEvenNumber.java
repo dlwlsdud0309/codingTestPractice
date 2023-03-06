d2129b893840fcef831ca52dd342b05c35e06c9a
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-06 23:59:34 +0900

    짝수의 합2

--- src/coding/test/practice/SumAnEvenNumber.java
+++ src/coding/test/practice/SumAnEvenNumber.java
@@ -3,20 +3,20 @@ package coding.test.practice;
 public class SumAnEvenNumber {
 	int solution(int n) {
 		int answer=0;
-		if (n%2==0) {
-			for (n = 2; n <= 1000; n++) {
+		
+		for (int i = 1; i <= n; i++) {
+			if (n%2==0) {
 				answer+=n;
-				System.out.println(answer);
+			}else {
+				System.out.println("홀수입니다");
 			}
-		}else {
-			System.out.println("홀수입니다");
 		}
 		
 		return answer;
 	}
 	public static void main(String[] args) {
 		SumAnEvenNumber saen=new SumAnEvenNumber();
-		int answer=saen.solution(4);
+		int answer=saen.solution(6);
 		System.out.println(answer);
 	}
 }
