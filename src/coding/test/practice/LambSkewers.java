c48d86300c59a5ea78ad153398729d86d694a198
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-08 17:46:58 +0900

    양꼬치 오답, 정답률 85.7%

--- src/coding/test/practice/LambSkewers.java
+++ src/coding/test/practice/LambSkewers.java
@@ -4,22 +4,20 @@ public class LambSkewers {
 	public int solution(int n, int k) {
         int answer = 0;
         
-        int lambSkewers=0;
         int lambSkewersPrice=12000;
-//        int drink=0;
         int drinkPrice=2000;
+        int x=n/10;
         
-     //   if (n%10==0) { //n이 10으로 나누어 떨어질 때마다
-			answer=n*lambSkewersPrice-(lambSkewers*drinkPrice)+drinkPrice*k;
-			System.out.println(answer);
-     //   }
+        if (n/10>0) { //n이 10으로 나누어 떨어질 때마다
+			answer=n*lambSkewersPrice-(x*drinkPrice)+drinkPrice*k;
+        }
         
         return answer;
     }
 	
 	public static void main(String[] args) {
 		LambSkewers ls=new LambSkewers();
-		int answer=ls.solution(64, 6);
+		int answer=ls.solution(10, 3);
 		System.out.println(answer);
 	}
 }
