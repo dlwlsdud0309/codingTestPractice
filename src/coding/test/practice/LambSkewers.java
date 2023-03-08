11c7480d4fab30370e5efcd47a031ff159bf32b1
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-08 18:04:43 +0900

    양꼬치 - 주문량, 즉 n이 10 이하일 경우 if문에서 탈락되는 것을 확인

--- src/coding/test/practice/LambSkewers.java
+++ src/coding/test/practice/LambSkewers.java
@@ -8,13 +8,13 @@ public class LambSkewers {
         int drinkPrice=2000;
         int x=n/10;
         
-        if (n/10>0) { //n이 10으로 나누어 떨어질 때마다
-			answer=n*lambSkewersPrice-(x*drinkPrice)+drinkPrice*k;
-        }
+//        if (n/10>0) { //n이 10으로 나누어 떨어질 때마다
+//			answer=n*lambSkewersPrice-(x*drinkPrice)+drinkPrice*k;
+//        }
         
 //      answer=n*12000+k*2000-(x*2000); //정답
         
-//        answer=n*lambSkewersPrice-(x*drinkPrice)+drinkPrice*k; //정답
+        answer=n*lambSkewersPrice-(x*drinkPrice)+drinkPrice*k; //정답
 //      if문이 오류였던 것 같다
         
         return answer;
@@ -22,7 +22,7 @@ public class LambSkewers {
 	
 	public static void main(String[] args) {
 		LambSkewers ls=new LambSkewers();
-		int answer=ls.solution(10, 3);
+		int answer=ls.solution(3, 3);
 		System.out.println(answer);
 	}
 }
