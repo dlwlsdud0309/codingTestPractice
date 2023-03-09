89e366e50a8676b97b5bd5081ed3a54e8708c07b
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-09 18:16:34 +0900

    머쓱이보다 키 큰 사람2, 배열에 랜덤 정수 넣기

--- src/coding/test/practice/MusscOfHeight.java
+++ src/coding/test/practice/MusscOfHeight.java
@@ -4,7 +4,12 @@ public class MusscOfHeight {
 	public int solution(int[] array, int height) {
         int answer = 0;
         
+        array=new int[3];
+        for (int i = 0; i < array.length; i++) {
+			array[i]=(int)(Math.random()*200+1);
+		}
         
+//        return answer;
         return answer;
     }
 }
