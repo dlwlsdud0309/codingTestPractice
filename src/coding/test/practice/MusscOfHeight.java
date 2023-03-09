9b135a58c5848f2a10387e1b0db01dd73062612f
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-09 18:10:06 +0900

    머쓱이보다 키 큰 사람

---
+++ src/coding/test/practice/MusscOfHeight.java
@@ -0,0 +1,10 @@
+package coding.test.practice;
+
+public class MusscOfHeight {
+	public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        
+        return answer;
+    }
+}
