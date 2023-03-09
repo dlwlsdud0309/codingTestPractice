6b0c04fae7b1eea7abcde7e795d85cb44da584b1
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-09 23:49:07 +0900

    머쓱이보다 키 큰 사람 진행중

--- src/coding/test/practice/MusscOfHeight.java
+++ src/coding/test/practice/MusscOfHeight.java
@@ -1,15 +1,31 @@
 package coding.test.practice;
 
+import java.util.Arrays;
+
 public class MusscOfHeight {
 	public int solution(int[] array, int height) {
         int answer = 0;
+        array=new int[6];
         
-        array=new int[3];
+      //  array=new int[3];
         for (int i = 0; i < array.length; i++) {
-			array[i]=(int)(Math.random()*200+1);
-		}
+        	int num=(int)(Math.random()*200)+1;
+        	array[i]=num;
+        	
+			for (int j = 0; j < i; j++) {
+				if (array[i]==num) {
+					i--;
+					break;
+				}
+			}
+        }
+        System.out.println(Arrays.toString(array));
         
 //        return answer;
         return answer;
     }
+	public static void main(String[] args) {
+		MusscOfHeight mog=new MusscOfHeight();
+		mog.solution(6, 200);
+	}
 }
