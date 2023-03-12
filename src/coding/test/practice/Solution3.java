02212a192b8ef97eb33ea22362ed12cdefa09538
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-13 00:14:22 +0900

    점의 위치 구하기2

--- src/coding/test/practice/Solution3.java
+++ src/coding/test/practice/Solution3.java
@@ -2,7 +2,16 @@ package coding.test.practice;
 
 public class Solution3 {
 	 public int solution(int[] dot) {
-	        dot= {};
+	     int answer=0;
+	     dot=new int[2];
+	     
+	     x=1;
+		 dot[0]='x';
+	     dot[1]='y';
+	     if ('x'>0) {
+			
+		}
+	        
 	        return answer;
 	    }
 }
