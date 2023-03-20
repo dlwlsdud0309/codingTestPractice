2d28d0f6a058052f02690b73eec43bec477d1fcb
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-20 17:54:37 +0900

    int number=1;은 while문의 밖에 작성해야함

--- src/practice/test/board/JavaBoard_clone5.java
+++ src/practice/test/board/JavaBoard_clone5.java
@@ -1,7 +1,74 @@
 package practice.test.board;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class JavaBoard_clone5 {
 	HashMap<String, Object> data=new HashMap<String, Object>();
+	ArrayList<HashMap<String, Object>> list=new ArrayList<HashMap<String,Object>>();
+	Scanner sc=new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("JavaBoard load");
+		JavaBoard_clone5 jc=new JavaBoard_clone5();
+		jc.seeBoard();
+	}
+	
+	private void seeBoard() {
+		int number=1; //while문 밖에 써야함
+		while(true) {
+			System.out.println("=============================");
+			System.out.println("NO\tTITLE\tWRITER\tDATE");
+			System.out.println("=============================");
+
+//			int number=1; //오답
+			for (int i = list.size()-1; i >=0; i--) {
+				System.out.print(list.get(i).get("번호"));
+				System.out.print("\t"+list.get(i).get("제목"));
+				System.out.print("\t"+list.get(i).get("작성자"));
+				System.out.print("\t"+list.get(i).get("작성일"));
+				System.out.println();
+				System.out.println("--------------------------");
+			}
+			System.out.println("1.조회\t2.등록\0.종료");
+			int input=sc.nextInt();
+			
+			switch (input) {
+			case 1:
+				System.out.println("조회할 게시물 번호");
+				break;
+			case 2:
+				putData(number);
+				number++;
+				list.add(data);
+				System.out.println("작성하신 글이 등록되었습니다.");
+				break;
+			case 0:
+				
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+	private void putData(int number) {
+		data=new HashMap<String, Object>();
+		
+		data.put("번호", number);
+		
+		System.out.println("제목 : ");
+		data.put("제목", sc.next());
+		System.out.println("내용 : ");
+		data.put("내용", sc.next());
+		System.out.println("작성자 : ");
+		data.put("작성자", sc.next());
+		
+		Date date=new Date();
+		SimpleDateFormat format=new SimpleDateFormat("yy-MM-dd");
+		data.put("작성일", format.format(date));
+	}
 }
