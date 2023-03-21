4f99101dc70dd98a3f3098dc9925a04a7bbb2b8e
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-21 10:08:59 +0900

    clone2, 조회, 수정, 삭제 구현

--- src/practice/test/board/JavaBoard_clone2.java
+++ src/practice/test/board/JavaBoard_clone2.java
@@ -39,6 +39,8 @@ public class JavaBoard_clone2 {
 			switch (input) {
 			case 1:
 				System.out.println("조회할 게시글 번호");
+				int input1=sc.nextInt();
+				listData(input1);
 				break;
 			case 2:
 				putData(number);
@@ -47,12 +49,65 @@ public class JavaBoard_clone2 {
 				System.out.println("작성한 글이 등록되었습니다");
 				break;
 			case 0:
+				System.out.println("JavaBoard를 종료합니다.");
+				System.exit(0);
 				break;
 			default:
 				break;
 			}
 		}
 	}
+	private void listData(int input1) { //게시물을 조회하고 상세정보 확인하기
+		for (int i = 0; i < list.size(); i++) {
+			if ((int)list.get(i).get("번호")==input1) {
+				System.out.println("제목 : "+list.get(i).get("제목"));
+				System.out.println("내용 : "+list.get(i).get("내용"));
+				System.out.println("작성자 : "+list.get(i).get("작성자"));
+				System.out.println("작성일 : "+list.get(i).get("작성일"));
+			
+				seeDetail(input1);
+				
+				return;
+			}
+		}
+	}
+	
+	private void seeDetail(int input1) {
+		while(true) {
+			System.out.println("1.수정\t2.삭제\t3.목록");
+			int input=sc.nextInt();
+			switch (input) {
+			case 1:
+				for (int i = 0; i < list.size(); i++) {
+					if ((int)list.get(i).get("번호")==input1) {
+						System.out.println("글을 수정합니다.");
+						
+						System.out.println("제목 : ");
+						list.get(i).put("제목", sc.next());
+						System.out.println("내용 : ");
+						list.get(i).put("내용", sc.next());
+						System.out.println("작성자 : ");
+						list.get(i).put("작성자", sc.next());
+					}
+				}
+				break;
+			case 2:
+				for (int i = 0; i < list.size(); i++) {
+					if ((int)list.get(i).get("번호")==input1) {
+						System.out.println("글을 삭제합니다.");
+						list.remove(i);
+					}
+				}
+				return;
+			case 3:
+				return;
+
+			default:
+				break;
+			}
+		}
+	}
+	
 	private void putData(int number) {
 		data=new HashMap<String, Object>(); //미작성
 		
