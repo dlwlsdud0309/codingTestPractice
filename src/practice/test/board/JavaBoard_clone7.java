b31f9bbebeb9f65ad9d6f10e896d904e6b472e33
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-25 17:41:16 +0900

    clone7 진행 중

--- src/practice/test/board/JavaBoard_clone7.java
+++ src/practice/test/board/JavaBoard_clone7.java
@@ -1,5 +1,46 @@
 package practice.test.board;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class JavaBoard_clone7 {
+	HashMap<String, Object> data=new HashMap<String, Object>();
+	ArrayList<HashMap<String, Object>> list=new ArrayList<HashMap<String,Object>>();
+	Scanner sc=new Scanner(System.in);
 	
+	public static void main(String[] args) {
+		System.out.println("JavaBoard load");
+		JavaBoard_clone7 jc=new JavaBoard_clone7();
+		jc.seeBoard();
+	}
+	
+	private void seeBoard() {
+		int number=1;
+		
+		while(true) {
+			System.out.println("=========================");
+			System.out.println("1.조회\t2.등록\t0/종료");
+			System.out.println("=========================");
+		
+			int input=sc.nextInt();
+			switch (input) {
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 0:
+				
+				break;
+
+			default:
+				break;
+			}
+
+		}
+		
+		
+	}
 }
