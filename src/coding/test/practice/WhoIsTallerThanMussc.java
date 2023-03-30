4b17542b5da28a35e7bcbac57d5917a7cb76888a
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-03-30 14:59:26 +0900

    머쓱이보다 키 큰 사람2

--- src/coding/test/practice/WhoIsTallerThanMussc.java
+++ src/coding/test/practice/WhoIsTallerThanMussc.java
@@ -6,31 +6,32 @@ import java.util.Collections;
 public class WhoIsTallerThanMussc {
 	public int solution(int[] array, int height) {
         int answer = 0;
-        array= new int[] {180,120,65};
+//        array= new int[] {180,120,65};
         for (int i = 0; i < array.length; i++) {
-			if(height<array[i]) {
-				answer=Collections.frequency(Arrays.asList(array), height<array[i]);
-			}
+//			if(height<array[i]) {
+////				answer=Collections.frequency(Arrays.asList(array), height<array[i]);
+//				answer=array.length;
+//			}
 		}
         System.out.println(answer);
         return answer;
     }
 	
 	public static void main(String[] args) {
-		int answer = 0;
-        int[] array= new int[] {180,120,65};
-        int height=165;
-        for (int i = 0; i < array.length; i++) {
-			if(height<array[i]) {
+//		int answer = 0;
+//        int[] array= new int[] {180,120,65};
+//        int height=165;
+//        for (int i = 0; i < array.length; i++) {
+//			if(height<array[i]) {
 //				answer=Collections.frequency(Arrays.asList(array), height<array[i]);
-			}
-		}
-        System.out.println(answer);
+//			}
+//		}
+//        System.out.println(answer);
 //        return answer;
 		
-//		WhoIsTallerThanMussc wittm=new WhoIsTallerThanMussc();
-//		int[] array= {180,120,65};
-//		int array2=wittm.solution(array, 165);
-//		System.out.println(array2);
+		WhoIsTallerThanMussc wittm=new WhoIsTallerThanMussc();
+		int[] array= {180,120,65};
+		int array2=wittm.solution(array, 165);
+		System.out.println(array2);
 	}
 }
