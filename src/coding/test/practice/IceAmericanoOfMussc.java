99f69b25ea21d734eb3c06ec05408f5247378a35
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-01 12:10:26 +0900

    아이스 아메리카노

---
+++ src/coding/test/practice/IceAmericanoOfMussc.java
@@ -0,0 +1,20 @@
+package coding.test.practice;
+
+import java.util.Arrays;
+
+public class IceAmericanoOfMussc {
+	public int[] solution(int money) {
+        int[] answer = {};
+        for (int i = 0; i < answer.length; i++) {
+			if (answer[i]%5500==0) {
+				
+			}
+		}
+        return answer;
+    }
+	public static void main(String[] args) {
+		IceAmericanoOfMussc iaom=new IceAmericanoOfMussc();
+		int[] answer=iaom.solution(0);
+		System.out.println(Arrays.toString(answer));
+	}
+}
