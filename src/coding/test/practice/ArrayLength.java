c428dc5022c582a8a0a6163248672ee69bd904ad
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-02 18:00:45 +0900

    배열 원소의 길이

---
+++ src/coding/test/practice/ArrayLength.java
@@ -0,0 +1,21 @@
+package coding.test.practice;
+
+import java.util.Arrays;
+
+public class ArrayLength {
+	public int[] solution(String[] strlist) {
+        int[] answer = {};
+        
+        for (int i = 0; i < strlist.length; i++) {
+			answer[i]=strlist[i].length();
+		}
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+		ArrayLength al=new ArrayLength();
+		String[] strlist= {"We", "are", "the", "world!"};
+		int[] answer=al.solution(strlist);
+		System.out.println(Arrays.toString(answer));
+	}
+}
