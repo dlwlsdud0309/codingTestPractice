f20b9f3be68d59ddfba3e5a1982572ba77485fe7
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-02 18:14:20 +0900

    배열 원소의 길이3, int[] answer = new int [strlist.length]; 사용

--- src/coding/test/practice/ArrayLength.java
+++ src/coding/test/practice/ArrayLength.java
@@ -4,21 +4,11 @@ import java.util.Arrays;
 
 public class ArrayLength {
 	public int[] solution(String[] strlist) {
-        int[] answer = {};
-//        strlist= new String[] {"We", "are", "the", "world!"};
+        int[] answer = new int [strlist.length];
         
-//        for (int i = 0; i < strlist.length; i++) {
-//			answer[i]=strlist[i].length();
-//		}
-        
-        for (String str : strlist) {
-			for (int i = 0; i < answer.length; i++) {
-				answer[i]=str.length();
-				System.out.println(answer[i]);
-			}
+        for (int i = 0; i < strlist.length; i++) {
+			answer[i]=strlist[i].length();
 		}
-			
-//        System.out.println(answer);
         
         return answer;
     }
