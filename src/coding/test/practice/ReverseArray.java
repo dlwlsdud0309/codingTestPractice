a9bc53f01a72a71d7ae1d2b555a9b6cd8f2cfd0a
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-02 22:46:39 +0900

    배열 뒤집기4, answer[i] = num_list[answer.length - 1 - i];에 대하여 이해 필요

--- src/coding/test/practice/ReverseArray.java
+++ src/coding/test/practice/ReverseArray.java
@@ -5,9 +5,12 @@ import java.util.Arrays;
 public class ReverseArray {
 	public int[] solution(int[] num_list) {
 		int[] answer= new int[num_list.length];
-		for (int i = num_list.length-1, j=0; i>=0; i--, j++) {
-			answer[j]=num_list[i];
-		}
+//		for (int i = num_list.length-1, j=0; i>=0; i--, j++) {
+//			answer[j]=num_list[i];
+//		}
+		for(int i = 0; i < answer.length; i++){
+            answer[i] = num_list[answer.length - 1 - i];
+        }
 		return answer;
     }
 	public static void main(String[] args) {
