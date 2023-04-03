0eab6ccbafeacb0b3d40b6a47b2add2306bc487a
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-03 22:21:46 +0900

    최빈값 구하기2

--- src/coding/test/practice/ToFindTheMode.java
+++ src/coding/test/practice/ToFindTheMode.java
@@ -3,14 +3,14 @@ package coding.test.practice;
 public class ToFindTheMode {
 	public int solution(int[] array) {
         int answer = 0;
-        
+        answer=array[0];
         for (int i = 0; i < array.length; i++) {
         	if (i<=0) {
 				answer=array[i];
 			}else if (i>0) { //i가 둘 이상인 경우
 				for (int j = 0; j < array.length; j++) {
 					if (array[i]==array[j]) {
-//						answer=array[i]==array[j];
+						answer=array[i];
 					}
 				}
 			}
