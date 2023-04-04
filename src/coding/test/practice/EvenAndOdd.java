d53164284fb45cb1bd7ab27e1319e487ee174a0f
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-04 23:09:20 +0900

    짝수 홀수 개수2, 변수 int even, odd를 사용하지 않아도 가능하다.

--- src/coding/test/practice/EvenAndOdd.java
+++ src/coding/test/practice/EvenAndOdd.java
@@ -7,23 +7,23 @@ public class EvenAndOdd {
         int[] answer = {};
         answer=new int[2];
         
-        int even=0;
-        int odd=0;
+//        int even=0;
+//        int odd=0;
         
         for (int i = 0; i < num_list.length; i++) {
 			if (num_list[i]%2==0) {
-				even++;
-				answer[0]=even;
+//				even++;
+				answer[0]++;
 			}else{
-				odd++;
-				answer[1]=odd;
+//				odd++;
+				answer[1]++;
 			}
 		}
         return answer;
     }
 	public static void main(String[] args) {
 		EvenAndOdd eao=new EvenAndOdd();
-		int[] num_list= {1, 3, 5, 7};
+		int[] num_list= {1, 2, 3, 4, 5};
 		int[] answer=eao.solution(num_list);
 		System.out.println(Arrays.toString(answer));
 	}
