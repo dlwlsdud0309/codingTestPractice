ba0fde0238e700d025ae7e7ec60c1ce838fd58cf
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-04 16:14:53 +0900

    아이스 아메리카노2, 코드정리 필요

--- src/coding/test/practice/IceAmericanoOfMussc.java
+++ src/coding/test/practice/IceAmericanoOfMussc.java
@@ -4,17 +4,21 @@ import java.util.Arrays;
 
 public class IceAmericanoOfMussc {
 	public int[] solution(int money) {
-        int[] answer = {};
+        int[] answer = new int[2];
         for (int i = 0; i < answer.length; i++) {
-			if (answer[i]%5500==0) {
-				
+			if (money%5500==0) {
+				answer[0]=money/5500;
+				answer[1]=money-(answer[0]*5500);
+			}else if (money-5500!=0) {
+				answer[0]=money/5500;
+				answer[1]=money-(answer[0]*5500);
 			}
 		}
         return answer;
     }
 	public static void main(String[] args) {
 		IceAmericanoOfMussc iaom=new IceAmericanoOfMussc();
-		int[] answer=iaom.solution(0);
+		int[] answer=iaom.solution(20000);
 		System.out.println(Arrays.toString(answer));
 	}
 }
