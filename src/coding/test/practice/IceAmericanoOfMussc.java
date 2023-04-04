824ad4d84dcfb0cf50e9b0750c1ba183f99d13ee
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-04 16:16:41 +0900

    아이스 아메리카노3, 코드 정리

--- src/coding/test/practice/IceAmericanoOfMussc.java
+++ src/coding/test/practice/IceAmericanoOfMussc.java
@@ -5,20 +5,19 @@ import java.util.Arrays;
 public class IceAmericanoOfMussc {
 	public int[] solution(int money) {
         int[] answer = new int[2];
-        for (int i = 0; i < answer.length; i++) {
-			if (money%5500==0) {
-				answer[0]=money/5500;
-				answer[1]=money-(answer[0]*5500);
-			}else if (money-5500!=0) {
-				answer[0]=money/5500;
-				answer[1]=money-(answer[0]*5500);
-			}
-		}
+        answer[0]=money/5500;
+		answer[1]=money-(answer[0]*5500);
+//			if (money%5500==0) {
+//				
+//			}else if (money-5500!=0) {
+//				answer[0]=money/5500;
+//				answer[1]=money-(answer[0]*5500);
+//		}
         return answer;
     }
 	public static void main(String[] args) {
 		IceAmericanoOfMussc iaom=new IceAmericanoOfMussc();
-		int[] answer=iaom.solution(20000);
+		int[] answer=iaom.solution(15000);
 		System.out.println(Arrays.toString(answer));
 	}
 }
