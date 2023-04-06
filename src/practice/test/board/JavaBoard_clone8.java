89e0e1ee1183febdadbb198adc28c34129da8acf
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-07 00:18:37 +0900

    JavaBoard_clone8 진행 중

--- src/practice/test/board/JavaBoard_clone8.java
+++ src/practice/test/board/JavaBoard_clone8.java
@@ -16,13 +16,35 @@ public class JavaBoard_clone8 {
 	}
 	
 	public void seeBoard() {
-		int input=sc.nextInt();
+		int number=1;
 		while(true) {
 			System.out.println("==================================");
 			System.out.println("NO\tTITLE\tWRITER\tDATE");
 			System.out.println("==================================");
 		
-			
+			System.out.println("1.조회\t2.등록\t0.종료");
+			int input=sc.nextInt();
+			switch (input) {
+			case 1:
+				System.out.println("조회할 게시물 번호 : ");
+				return;
+			case 2:
+				System.out.println("글을 등록합니다.");
+				number++;
+				putData(number);
+				return;
+			case 0:
+				System.exit(0);
+				break;
+
+			default:
+				break;
+			}
 		}
+	}
+	public void putData(int number) {
+		data=new HashMap<String, Object>();
+		
+		data.put("번호", number);
 	}
 }
