29942f0dd5f4a21ef3fa097a1acac39ecce5668f
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-07 22:47:41 +0900

    n의 배수 고르기

---
+++ src/coding/test/practice/FindMultiplesOfN.java
@@ -0,0 +1,20 @@
+package coding.test.practice;
+
+import java.util.Arrays;
+
+public class FindMultiplesOfN {
+	public int[] solution(int n, int[] numlist) {
+        int[] answer = {};
+        int[] arr=
+        
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		FindMultiplesOfN fmon=new FindMultiplesOfN();
+		int[] numlist= {4, 5, 6, 7, 8, 9, 10, 11, 12};
+		int[] answer=fmon.solution(3, numlist);
+		System.out.println(Arrays.toString(answer));
+	}
+}
