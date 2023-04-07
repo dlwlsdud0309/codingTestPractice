c0997bc0abefbf49c9e27af2aaa159dd31f12983
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-07 23:39:36 +0900

    n의 배수 고르기2

--- src/coding/test/practice/FindMultiplesOfN.java
+++ src/coding/test/practice/FindMultiplesOfN.java
@@ -5,7 +5,27 @@ import java.util.Arrays;
 public class FindMultiplesOfN {
 	public int[] solution(int n, int[] numlist) {
         int[] answer = {};
-        int[] arr=
+        int j=0;
+        for (int i = 0; i < numlist.length; i++) {
+        	if (numlist[i]%n==0) {
+        		j++;
+        		answer=new int[j];
+        		System.out.println("numlist[i] : "+numlist[i]);
+        		System.out.println(answer.length);
+        		System.out.println("answer : "+Arrays.toString(answer));
+
+        		
+        	}
+//        		numlist=new int[answer.length];
+//        		System.out.println(numlist.length);
+		}
+        for (int i = 0; i < numlist.length; i++) {
+        	if (numlist[i]%n==0) {
+    			for (int k = 0; k < answer.length; k++) {
+					answer[k]=numlist[i];
+				}
+			}
+		}
         
         
         return answer;
