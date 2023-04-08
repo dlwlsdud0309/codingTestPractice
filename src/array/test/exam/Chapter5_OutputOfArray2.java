745443ed49b8ab5f25d146e13c484605dad8efd5
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-09 00:43:22 +0900

    char배열은 println메서드로 출력하면 각 요소가 구분자 없이 그대로 출력된다, println메서드가 char배열일 때만 이렇게 동작한다. 배열의 선언과 생성을 따로하는 경우 new int[]는 생략할 수 없다.

--- src/array/test/exam/Chapter5_OutputOfArray2.java
+++ src/array/test/exam/Chapter5_OutputOfArray2.java
@@ -8,8 +8,11 @@ public class Chapter5_OutputOfArray2 {
 		int[] iArr1=new int[10];
 		int[] iArr2=new int[10];
 //		int[] iArr3=new int[] {100,95,80,70,60};
-		int[] iArr3= {100,95,80,70,60};
+		int[] iArr3= {100,95,80,70,60}; //new int[]를 생략할 수 있음
 		char[] chArr= {'a','b','c','d'};
+		
+//		int[] score;
+//		score= {50,60,70,80,90}; //new int[]를 생략할 수 없음
 		
 		for (int i = 0; i < iArr1.length; i++) {
 			iArr1[i]=i+1; //1~10의 숫자를 순서대로 배열에 넣는다.
