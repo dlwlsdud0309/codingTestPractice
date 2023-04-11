e9bdcea846dfb9063267c11c7ed997188d2bca33
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-11 21:41:23 +0900

    자바의 정석, 배열의 활용(3) - 섞기(shuffle)3

---
+++ src/array/test/exam/Chapter5_OutputOfArray3.java
@@ -0,0 +1,17 @@
+package array.test.exam;
+
+import java.util.Arrays;
+
+public class Chapter5_OutputOfArray3 {
+	public static void main(String[] args) {
+		int[] numArr= {0,1,2,3,4,5,6,7,8,9};
+		
+		for (int i = 0; i < 2; i++) {
+			int n=(int)(Math.random()*10);
+			int tmp=numArr[0];
+			numArr[0]=numArr[n];
+			numArr[n]=tmp;
+		}
+		System.out.println(Arrays.toString(numArr));
+	}
+}
