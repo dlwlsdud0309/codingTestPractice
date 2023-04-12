3e29bb6dff3dfd5d7bca8edd29161d6ba5106bfe
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-12 15:50:52 +0900

    자바의 정석, 배열의 활용(4) - 로또 번호 만들기2

---
+++ src/array/test/exam/Chapter5_LottoNumber2.java
@@ -0,0 +1,30 @@
+package array.test.exam;
+
+public class Chapter5_LottoNumber2 {
+	public static void main(String[] args) {
+		int[] ball=new int[45]; //45개의 정수값을 저장하기 위한 배열 생성
+		
+		//배열의 각 요소에 1~45의 값을 저장한다
+		for (int i = 0; i < ball.length; i++) {
+			ball[i]=i+1; //ball[0]에 1이 저장된다
+		}
+		
+		int tmp=0; //두 값을 바꾸는데 사용할 임시변수
+		int j=0; //임의의 값을 얻어서 저장할 변수
+		
+		//배열의 i번째 요소와 임의의 요소에 저장된 값을 서로 바꿔서 값을 섞는다
+		//0번째부터 5번째 요소까지 모두 6개만 바꾼다
+		for (int i = 0; i < 6; i++) {
+			j=(int)(Math.random()*45); //0~44범위의 임의의 값을 얻는다
+			tmp=ball[i];
+			ball[i]=ball[j];
+			ball[j]=tmp;
+		}
+		
+		//배열 ball의 앞에서부터 6개의 요소를 출력헌다
+		//1부터 45까지의 번호가 쓰인 카드를 잘 섞은 다음 맨 위의 6장을 꺼내는 것과 같다
+		for (int i = 0; i < 6; i++) {
+			System.out.printf("ball[%d]=%d%n", i,ball[i]);
+		}
+	}
+}
