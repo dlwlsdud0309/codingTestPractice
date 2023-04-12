73cee6631fd4375ac6bda89302b5e195716efaa8
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-12 15:54:17 +0900

    자바의 정석, String배열의 선언과 생성

---
+++ src/array/test/exam/Chapter5_StringArray.java
@@ -0,0 +1,20 @@
+package array.test.exam;
+
+public class Chapter5_StringArray {
+	public static void main(String[] args) {
+		//String배열의 선언과 생성
+		//p.141
+		String[] name=new String[3]; //3개의 문자열을 담을 수 있는 배열을 생성한다
+		//참조형 변수의 기본값은 null이다, null은 어떤 객체도 가리키고 있지 않다는 뜻이다
+		
+		//변수 타입에 따른 기본값
+//		boolean - false
+//		char - \u0000
+//		byte, short, int - 0
+//		long - 0L
+//		float - 0.0f
+//		double - 0.0d 또는 0.0
+//		참조형 - null
+		
+	}
+}
