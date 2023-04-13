ec4a07b10225fe842687f3455429f6ea6f73ef5c
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-13 17:44:23 +0900

    행index의 범위는 0~행의 길이-1, 열index의 범위는 0~열의 길이-1

--- src/array/test/exam/Chapter5_IndexOfTwoDimensionalArray.java
+++ src/array/test/exam/Chapter5_IndexOfTwoDimensionalArray.java
@@ -7,6 +7,9 @@ public class Chapter5_IndexOfTwoDimensionalArray {
 		
 		//배열이름[행index][열index] 예)score[3][2]
 		
+		//행index의 범위는 '0~행의 길이 -1'
+		//열index의 범위는 '0~열의 길이 -1'
+		
 		int[][] score=new int[4][3]; //4행3열의 2차원 배열 score를 생성
 		
 		//배열 score의 1행 1열에 100을 저장
