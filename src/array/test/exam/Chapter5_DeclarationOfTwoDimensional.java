dabb8930aa283ab35e4c36569a9ebae39b786c2b
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-13 17:35:33 +0900

    자바의 정석, 2차원 배열의 선언

---
+++ src/array/test/exam/Chapter5_DeclarationOfTwoDimensional.java
@@ -0,0 +1,16 @@
+package array.test.exam;
+
+public class Chapter5_DeclarationOfTwoDimensional {
+	public static void main(String[] args) {
+		//2차원 배열의 선언
+		//p.147
+		
+		//2차원 배열을 선언하는 방법
+		int[][] score;
+		
+		//4행3열의 데이터
+		int[][] score2=new int[4][3]; //4행3열의 2차원 배열 생성
+		//이 경우 int의 기본값인 0이 저장된다
+		//참조형의 경우 null이 저장된다
+	}
+}
