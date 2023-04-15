f8ce614f8efb06a630261c3a4d54cfb722e52093
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-15 20:03:06 +0900

    JavaBoard clone9 진행중

--- src/practice/test/board/JavaBoard_clone9.java
+++ src/practice/test/board/JavaBoard_clone9.java
@@ -5,11 +5,20 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class JavaBoard_clone9 {
-	HashMap<String, Object> data=new HashMap<String, Object>;
+	HashMap<String, Object> data=new HashMap<String, Object>();
 	ArrayList<HashMap<String, Object>> list=new ArrayList<HashMap<String,Object>>();
 	Scanner sc=new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		
+		System.out.println("JavaBoard load");
+		JavaBoard_clone9 jc=new JavaBoard_clone9();
+		jc.seeBoard();
+	}
+	
+	public void seeBoard() {
+		int number=1;
+		System.out.println("===============================================");
+		System.out.println("");
+		System.out.println("===============================================");
 	}
 }
