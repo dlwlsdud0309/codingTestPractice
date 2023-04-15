ac95de128e1ed7a009165f1fd35da74a9a089db6
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-15 19:46:03 +0900

    JavaBoard clone9

---
+++ src/practice/test/board/JavaBoard_clone9.java
@@ -0,0 +1,15 @@
+package practice.test.board;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class JavaBoard_clone9 {
+	HashMap<String, Object> data=new HashMap<String, Object>;
+	ArrayList<HashMap<String, Object>> list=new ArrayList<HashMap<String,Object>>();
+	Scanner sc=new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	}
+}
