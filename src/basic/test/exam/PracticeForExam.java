3d45e8918c7c4994fd743d8e9f15b831f6c59f00
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-19 01:03:51 +0900

    for문에 감소연산자 사용, 10에서 시작해 0(i>=0)까지 출력하기

---
+++ src/basic/test/exam/PracticeForExam.java
@@ -0,0 +1,10 @@
+package basic.test.exam;
+
+public class PracticeForExam {
+	public static void main(String[] args) {
+		int i=0;
+		for (i = 10; i >= 0; i--) { //10부터 0가 0과 같거나 클때까지 1씩감소
+			System.out.println(i);
+		}
+	}
+}
