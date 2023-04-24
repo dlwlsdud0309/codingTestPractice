03944f11c08b838fc814eead18767c329f3706b8
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-04-24 10:27:41 +0900

    clone6 putData

--- src/practice/test/board/JavaBoard_clone6.java
+++ src/practice/test/board/JavaBoard_clone6.java
@@ -1,6 +1,8 @@
 package practice.test.board;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -53,6 +55,19 @@ public class JavaBoard_clone6 {
 	}
 	
 	public void putData(int number) {
+		data=new HashMap<String, Object>();
 		
+		data.put("번호", number);
+		
+		System.out.println("제목 : ");
+		data.put("제목", sc.next());
+		System.out.println("내용 : ");
+		data.put("내용", sc.next());
+		System.out.println("작성자 : ");
+		data.put("작성자", sc.next());
+		
+		Date date=new Date();
+		SimpleDateFormat format=new SimpleDateFormat("yy-MM-dd");
+		data.put("작성일", format.format(date));
 	}
 }
