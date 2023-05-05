c6d336dbb61e5b3347664cc6697f2623143f192b
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-05-05 12:33:29 +0900

    중간 세이브, JTextField 위치 지정, JFrame 실행 시 중앙 정렬, 크기조정 불가

--- src/practice/test/cal/Calculator.java
+++ src/practice/test/cal/Calculator.java
@@ -1,10 +1,27 @@
 package practice.test.cal;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class Calculator extends JFrame{
 	
+	private JTextField inputSpace;
+	
 	public Calculator() {
+		setLayout(null); //별다른 레이아웃 관리자를 사용하지 않을 것이므로 기본으로 있는 레이아웃 관리자를 제거
+		
+		inputSpace=new JTextField();
+		inputSpace.setEditable(false); //버튼으로만 입력받도록 편집가능여부는 불가능으로 설정
+		inputSpace.setBackground(Color.white);
+		inputSpace.setHorizontalAlignment(JTextField.RIGHT);
+		inputSpace.setFont(new Font("Arial", Font.BOLD, 50));
+		inputSpace.setBounds(8, 10, 270, 70); //x:8, y:10 위치에 270x70의 크기를 의미함
+		
+		add(inputSpace);
+		
 		setTitle("계산기");
 		setVisible(true);
 		setSize(300,370);
