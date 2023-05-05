61cd46c8038cae7351124781b0219ee1cbfb84d7
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-05-05 18:17:11 +0900

    계산기 실행하여 GUI 정상출력 확인

--- src/practice/test/cal/CalExam_clone1.java
+++ src/practice/test/cal/CalExam_clone1.java
@@ -13,16 +13,21 @@ public class CalExam_clone1 extends JFrame{
 
 	private JPanel panel;
 	private JTextField tField;
+	private JButton[] buttons;
+	private String[] button_names= {
+			"C","/","*","=","7","8","9",
+			"+","4","5","6","-","1","2","3","0"	
+		};
 	
 	public CalExam_clone1() {
-//		setLayout(null);
+		setLayout(null);
 		
 		setTitle("계산기");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		setSize(300, 370);
-		setVisible(true);
 		setLocationRelativeTo(null);
 		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		tField=new JTextField();
 		tField.setEditable(false);
@@ -32,23 +37,36 @@ public class CalExam_clone1 extends JFrame{
 		tField.setBounds(8,10,270,70); //x:8, y:10, 270x70
 		
 		panel=new JPanel();
-		panel.setLayout(new GridLayout(0, 5, 3, 3));
+		panel.setLayout(new GridLayout(0,4,10,10)); //차례대로 가로, 새로, 좌우간격, 상하간격
+		panel.setBounds(8, 90, 270, 235);
 		
-		JPanel panelA=new JPanel();
-		JPanel panelB=new JPanel();
 		
-		panelB.setLayout(new GridLayout(0,4,10,10)); //차례대로 가로, 새로, 좌우간격, 상하간격
-		panelB.setBounds(8, 90, 270, 235);
+		buttons=new JButton[button_names.length];
+		int index=0;
 		
-		String[] button_names= {
-				"C","/","*","=","7","8","9",
-				"+","4","5","6","-","1","2","3","0"	
-			};
-		JButton[] buttons=new JButton[button_names.length];
+		for (int i = 0; i < button_names.length; i++) {
+			
+			buttons[i]=new JButton(button_names[i]);
+			buttons[i].setFont(new Font("Arial",Font.BOLD,20));
+			
+			if(button_names[i]=="C") {
+				buttons[i].setBackground(Color.red);
+			}else if((i>=4 || i<=6) || (i>=8 || i<=10) || (i>=12 && i<=14)) {
+				buttons[i].setBackground(Color.black);
+			}else {
+				buttons[i].setBackground(Color.gray);
+			}
+			buttons[i].setForeground(Color.white);
+			buttons[i].setBorderPainted(false);
+			
+			panel.add(buttons[i]);
+			index++;
+		}
 	
-	
-		add("North",tField);
-		add("Center",panel);
+		add(tField);
+		add(panel);
+		
+		setVisible(true);
 	}
 	
 	public static void main(String[] args) {
