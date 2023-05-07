59f79c91de090e8c0cf1446079d9d929f9cb1173
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-05-07 22:15:02 +0900

    clone15 정상출력 확인

--- src/practice/test/board/JavaBoard_clone15.java
+++ src/practice/test/board/JavaBoard_clone15.java
@@ -25,12 +25,24 @@ public class JavaBoard_clone15 {
 			System.out.println("NO\tTITLE\tWRITER\tDATE");
 			System.out.println("===============================");
 		
+			//출력
+			for (int i = list.size()-1; i >= 0; i--) {
+				System.out.print(list.get(i).get("번호"));
+				System.out.print("\t"+list.get(i).get("제목"));
+				System.out.print("\t"+list.get(i).get("작성자"));
+				System.out.print("\t"+list.get(i).get("작성일"));
+				System.out.println();
+				System.out.println("---------------------");
+			}
+			
 			System.out.println("1.조회\t2.등록\t0.종료");
 			int input=sc.nextInt();
 			
 			switch (input) {
 			case 1:
 				System.out.println("조회할 게시글 번호 : ");
+				int input1=sc.nextInt();
+				seeDetail(input1);
 				break;
 			case 2:
 				putData(number);
@@ -48,6 +60,51 @@ public class JavaBoard_clone15 {
 		}
 	}
 	
+	private void seeDetail(int input1) {
+		System.out.println("글을 조회합니다.");
+		for (int i = 0; i < list.size(); i++) {
+			if ((int)list.get(i).get("번호")==input1) {
+				System.out.println("제목 : "+list.get(i).get("제목"));
+				System.out.println("내용 : "+list.get(i).get("내용"));
+				System.out.println("작성자 : "+list.get(i).get("작성자"));
+				System.out.println("작성일 : "+list.get(i).get("작성일"));
+			
+				modifyDeleteList(input1);
+			}
+		}
+	}
+	
+	private void modifyDeleteList(int input1) {
+		System.out.println("1.수정\t2.삭제\t3.목록");
+		int input=sc.nextInt();
+		switch (input) {
+		case 1:
+			System.out.println("글을 수정합니다.");
+			for (int i = 0; i < list.size(); i++) {
+				if ((int)list.get(i).get("번호")==input1) {
+					System.out.println("제목 : ");
+					list.get(i).put("제목", sc.next());
+					System.out.println("내용 : ");
+					list.get(i).put("내용", sc.next());
+				}
+			}
+			break;
+		case 2:
+			System.out.println("글을 삭제합니다.");
+			for (int i = 0; i < list.size(); i++) {
+				if ((int)list.get(i).get("번호")==input1) {
+					list.remove(i);
+				}
+			}
+			break;
+		case 3:
+			
+			return;
+
+		default:
+			break;
+		}
+	}
 	private void putData(int number) {
 		data=new HashMap<String, Object>();
 		
@@ -64,5 +121,6 @@ public class JavaBoard_clone15 {
 		
 		Date date=new Date();
 		SimpleDateFormat sdf=new SimpleDateFormat("yy-MM-dd");
+		data.put("작성일", sdf.format(date));
 	}
 }
