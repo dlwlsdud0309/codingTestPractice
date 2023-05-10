dd1b20604f516f499b9dc5d4a8168a04dea10e65
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-05-10 22:24:17 +0900

    계산기 clone3 진행 중

--- src/practice/test/cal/ChangeCalExam_clone3.java
+++ src/practice/test/cal/ChangeCalExam_clone3.java
@@ -1,5 +1,15 @@
 package practice.test.cal;
 
-public class ChangeCalExam_clone3 {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class ChangeCalExam_clone3 extends JFrame{
+	private JPanel panel;
+	private JTextField tField;
+	private JButton[] buttons;
+	private String[] button_names= {
+			
+	};
 }
