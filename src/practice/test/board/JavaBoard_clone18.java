c295cb92e97ff11dcc066b8d3531a304c83d0f77
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-05-18 22:35:14 +0900

    clone18 진행 중

--- src/practice/test/board/JavaBoard_clone18.java
+++ src/practice/test/board/JavaBoard_clone18.java
@@ -1,6 +1,8 @@
 package practice.test.board;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -23,6 +25,15 @@ public class JavaBoard_clone18 {
 			System.out.println("NO\tTITLE\tWRITER\tDATE");
 			System.out.println("====================================");
 		
+			for (int i = list.size()-1; i >= 0; i--) {
+				System.out.print(list.get(i).get("번호"));
+				System.out.print("\t"+list.get(i).get("제목"));
+				System.out.print("\t"+list.get(i).get("작성자"));
+				System.out.print("\t"+list.get(i).get("작성일"));
+				System.out.println();
+				System.out.println("--------------------------------------");
+			}
+			
 			System.out.println("1.조회\t2.등록\t0.종료");
 			int input = sc.nextInt();
 			switch (input) {
@@ -30,7 +41,10 @@ public class JavaBoard_clone18 {
 				
 				break;
 			case 2: //등록
-				
+				System.out.println("글을 등록합니다.");
+				putData(number);
+				number++;
+				list.add(data);
 				break;
 			case 0:
 				System.out.println("JavaBoard exit");
@@ -41,5 +55,24 @@ public class JavaBoard_clone18 {
 				break;
 			}
 		}
+	}
+	
+	private void putData(int number) {
+		data = new HashMap<String, Object>();
+		
+		data.put("번호", number);
+		
+		System.out.println("제목 : ");
+		data.put("제목", sc.next());
+
+		System.out.println("내용 : ");
+		data.put("내용", sc.next());
+
+		System.out.println("작성자 : ");
+		data.put("작성자", sc.next());
+
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");
+		data.put("작성일", sdf.format(date));
 	}
 }
