162f4768a77ae362fd13145dde9995ff9de97ac1
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-01 12:26:19 +0900

    계산기 clone5 형태 완성, 기능 추가 시작

--- src/practice/test/cal/ChangeCalExam_clone5.java
+++ src/practice/test/cal/ChangeCalExam_clone5.java
@@ -1,5 +1,9 @@
 package practice.test.cal;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -27,6 +31,51 @@ public class ChangeCalExam_clone5 extends JFrame{
 		setLocationRelativeTo(null); //실행 시 중앙에 위치
 		setResizable(false); //크기 조정 불가능
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//JTextField
+		tField = new JTextField();
+		tField.setEditable(false); //타이핑 불가
+		tField.setBackground(Color.white);
+		tField.setHorizontalAlignment(JTextField.RIGHT);
+		tField.setFont(new Font("Arial",Font.BOLD,50));
+		tField.setBounds(8, 10, 270, 70);
+		//x:8(오른쪽으로 이동), y:10(아래로 이동), 270(길이, 오른쪽으로 길어짐), 70(아래로 길어짐)
+		
+		add(tField);
+		
+		
+		//JPanel
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(0, 4, 10, 10)); //가로, 세로, 좌우간격, 상하간격
+		panel.setBounds(8, 90, 270, 235);
+		
+		add(panel);
+		
+		
+		//JButton, 길이지정
+		//int[] score;
+		//score = new int[length];
+		buttons = new JButton[button_names.length]; //private JButton[] buttons;
+		
+		//반복문 사용하여 버튼 부착
+		for (int i = 0; i < button_names.length; i++) {
+			buttons[i] = new JButton(button_names[i]);
+//			buttons[i].addActionListener(this);
+			buttons[i].setFont(new Font("Arial",Font.BOLD,20));
+		
+			//버튼에 색 지정
+			if (button_names[i]=="C") {
+				tField.setText("0.0");
+				buttons[i].setBackground(Color.red);
+			}else if((i>=4 && i<=6) || (i>=8 && i<=10) || (i>=12 && i<=14)) {
+				buttons[i].setBackground(Color.black);
+			}else {
+				buttons[i].setBackground(Color.gray);
+			}
+			buttons[i].setForeground(Color.white);
+			buttons[i].setBorderPainted(false); //테두리 없애기
+			panel.add(buttons[i]);
+		}
 		
 		setVisible(true);
 	}
