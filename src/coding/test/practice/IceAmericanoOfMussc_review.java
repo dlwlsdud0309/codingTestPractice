616b2cd26c12f4a1a677818cdda119b1a5086cbd
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-02 14:05:46 +0900

    코딩 테스트 - 아이스 아메리카노 작동 확인

--- src/coding/test/practice/IceAmericanoOfMussc_review.java
+++ src/coding/test/practice/IceAmericanoOfMussc_review.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 public class IceAmericanoOfMussc_review {
 	public int[] solution(int money) {
         int[] answer = new int[2];
-        
+        answer[0] = money/5500;
+        answer[1] = money-(5500*answer[0]);
         return answer;
     }
 	public static void main(String[] args) {
 		IceAmericanoOfMussc_review ir = new IceAmericanoOfMussc_review();
-		int[] answer = ir.solution(0);
+		int[] answer = ir.solution(15000);
 		System.out.println(Arrays.toString(answer));
 	}
 }
