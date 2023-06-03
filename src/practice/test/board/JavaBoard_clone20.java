71dd6459a20ac6dfde4062cd2f0a48736766b3da
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-03 11:10:34 +0900

    JavaBoard clone20시작

---
+++ src/practice/test/board/JavaBoard_clone20.java
@@ -0,0 +1,21 @@
+package practice.test.board;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class JavaBoard_clone20 {
+	HashMap<String, Object> data = new HashMap<String, Object>();
+	ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String,Object>>();
+	Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("JavaBoard load");
+		JavaBoard_clone20 jc = new JavaBoard_clone20();
+		jc.seeBoard();
+	}
+	
+	private void seeBoard() {
+		
+	}
+}
