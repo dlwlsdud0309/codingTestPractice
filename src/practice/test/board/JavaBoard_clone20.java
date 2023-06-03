5055e571852f732fd565e42664dca659eeb1cb19
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-03 11:13:29 +0900

    업로드 정상속도 확인, clone20 seeBoard 작성 중

--- src/practice/test/board/JavaBoard_clone20.java
+++ src/practice/test/board/JavaBoard_clone20.java
@@ -22,6 +22,22 @@ public class JavaBoard_clone20 {
 			System.out.println("===========================================");
 			System.out.println("NO\tTITLE\tWRITER\tDATE");
 			System.out.println("===========================================");
+			
+			int input = sc.nextInt();
+			switch (input) {
+			case 1: //조회
+				System.out.println("게시글을 조회합니다.");
+				
+				break;
+			case 2:
+				
+				break;
+			case 0: //종료
+				System.exit(0);
+				break;
+			default:
+				break;
+			}
 		}
 	}
 }
