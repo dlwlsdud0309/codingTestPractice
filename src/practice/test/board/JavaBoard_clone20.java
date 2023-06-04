1f8d33bd5c0a9a4927421253342f7bb2e1068f33
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-04 16:09:56 +0900

    putData에서 new 연산자를 사용하여 HashMap를 생성하지 않았기 때문에 Call by reference에 해당함(Call by value와 Call by reference의 차이)

--- src/practice/test/board/JavaBoard_clone20.java
+++ src/practice/test/board/JavaBoard_clone20.java
@@ -1,6 +1,8 @@
 package practice.test.board;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -23,14 +25,27 @@ public class JavaBoard_clone20 {
 			System.out.println("NO\tTITLE\tWRITER\tDATE");
 			System.out.println("===========================================");
 			
+			for (int i = list.size()-1; i >= 0; i--) {
+				System.out.print(list.get(i).get("번호"));
+				System.out.print("\t"+list.get(i).get("제목"));
+				System.out.print("\t"+list.get(i).get("작성자"));
+				System.out.print("\t"+list.get(i).get("작성일"));
+				System.out.println();
+				System.out.println("--------------------------------------");
+			}
+			
+			System.out.println("1.조회\t2.등록\0.종료");
 			int input = sc.nextInt();
 			switch (input) {
 			case 1: //조회
 				System.out.println("게시글을 조회합니다.");
 				
 				break;
-			case 2:
-				
+			case 2: //등록
+				System.out.println("글을 등록합니다.");
+				putData(number);
+				number++;
+				list.add(data);
 				break;
 			case 0: //종료
 				System.exit(0);
@@ -39,5 +54,24 @@ public class JavaBoard_clone20 {
 				break;
 			}
 		}
+	}
+	
+	private void putData(int number) {
+		data = new HashMap<String, Object>();
+		
+		data.put("번호", number);
+		
+		System.out.println("제목 : ");
+		data.put("제목", sc.next());
+		
+		System.out.println("내용 : ");
+		data.put("내용", sc.next());
+		
+		System.out.println("작성자 : ");
+		data.put("작성자", sc.next());
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");
+		data.put("작성일", sdf.format(date));
 	}
 }
