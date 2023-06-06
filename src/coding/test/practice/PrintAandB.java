6d8b54057c998b60a3eae03c0f0b26cd073e69dd
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-06 13:41:58 +0900

    코딩 기초 트레이닝 - a와 b 출력하기

---
+++ src/coding/test/practice/PrintAandB.java
@@ -0,0 +1,16 @@
+package coding.test.practice;
+
+import java.util.Scanner;
+
+public class PrintAandB {
+	//코딩 기초 테스트
+	//a와 b 출력하기
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
+}
--- src/coding/test/practice/PrintString.java
+++ src/coding/test/practice/PrintString.java
@@ -3,6 +3,8 @@ package coding.test.practice;
 import java.util.Scanner;
 
 public class PrintString {
+	//코딩 기초 트레이닝
+	//문자열 출력하기
     public static void main(String[] args) {
     	 Scanner sc = new Scanner(System.in);
          String a = sc.next();
