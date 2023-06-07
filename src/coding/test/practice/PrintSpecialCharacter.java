88c438effbeb2acc0a9f25a212d1794eec6c6c13
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-07 14:40:41 +0900

    코딩 기초 트레이닝 - 특수문자 출력하기

---
+++ src/coding/test/practice/PrintSpecialCharacter.java
@@ -0,0 +1,10 @@
+package coding.test.practice;
+
+public class PrintSpecialCharacter {
+	//코딩 기초 트레이닝
+	//특수문자 출력하기
+	
+	public static void main(String[] args) {
+		System.out.println("!@#$%^&*(\\'\"<>?:;");
+	}
+}
