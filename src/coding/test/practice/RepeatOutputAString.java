c06224b9eb193ed0da562fd16160f560544d1759
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-07 14:30:33 +0900

    코딩 기초 트레이닝 정답은 출력문에 출력되는 것만 정답으로 처리되는 것을 확인함

--- src/coding/test/practice/RepeatOutputAString.java
+++ src/coding/test/practice/RepeatOutputAString.java
@@ -7,9 +7,9 @@ public class RepeatOutputAString {
 	//문자열 반복해서 출력하기
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("str : ");
+        //System.out.println("str : ");
         String str = sc.next();
-        System.out.println("n : ");
+        //System.out.println("n : ");
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
         	System.out.print(str);
