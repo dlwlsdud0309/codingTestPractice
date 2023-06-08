82457242906c01e40503b8673143613b0a1a97f0
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-08 11:47:59 +0900

    코딩 기초 트레이닝 - 문자열 붙여서 출력하기

---
+++ src/coding/test/practice/PrintByPuttingAString.java
@@ -0,0 +1,14 @@
+package coding.test.practice;
+
+import java.util.Scanner;
+
+public class PrintByPuttingAString {
+	//코딩 기초 트레이닝
+	//문자열 붙여서 출력하기
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a+b);
+    }
+}
