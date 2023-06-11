4ccb52f0cb2862d5a34a06f4ec45bb013099ab3f
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-11 15:30:27 +0900

    코딩 기초 트레이닝 - flag에 따라 다른 값 반환하기, Syntax error on token "+", invalid AssignmentOperator 오류 확인

---
+++ src/coding/test/practice/ReturnResultValueAccordingToTheFlag.java
@@ -0,0 +1,24 @@
+package coding.test.practice;
+
+public class ReturnResultValueAccordingToTheFlag {
+	//코딩 기초 테스트
+	//flag에 따라 다른 값 반환하기
+	
+	public int solution(int a, int b, boolean flag) {
+        int answer = 0;
+        
+        if(flag=true) {
+        	a+b=answer;
+        }else {
+        	a-b=answer;
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		ReturnResultValueAccordingToTheFlag rrvattf = new ReturnResultValueAccordingToTheFlag();
+		int answer = rrvattf.solution(-4, 7, false);
+		System.out.println(answer);
+	}
+}
