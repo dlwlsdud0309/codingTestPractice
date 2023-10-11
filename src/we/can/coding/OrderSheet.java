cea1a459c00411e91c680cf18107bd5125e0e2e6
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-12 00:06:21 +0900

    객체 협력에 대하여, 주문서에 해당하는 orderSheet 클래스 작성

--- src/we/can/coding/OrderSheet.java
+++ src/we/can/coding/OrderSheet.java
@@ -2,6 +2,19 @@ package we.can.coding;
 
 public class OrderSheet {
 
-	public boolean isCompleted;
-	public int coffeeNum;
+	static int serialNum = 100; //고유번호
+	int orderNum; //주문번호
+	public int coffeeNum; //커피의 잔 수
+	public boolean isCompleted; //커피가 만들어졌는지에 대한 완료 여부
+	
+	public OrderSheet(int coffeeNum) {
+		this.coffeeNum = coffeeNum;
+		serialNum++;
+		orderNum = serialNum;
+		isCompleted = false;
+	}
+	
+	public void showInfo() {
+		System.out.println("주문서 번호는 "+orderNum+"이고, 커피수는 "+coffeeNum+"이며 완료 여부는 "+isCompleted+"입니다.");
+	}
 }
