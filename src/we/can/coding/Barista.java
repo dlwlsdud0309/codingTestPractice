bbf71867f70329117b733a29c5e35be943593731
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-11 23:30:30 +0900

    파라미터를 다르게 해서 생성자를 만들 때, 객체 초기화를 쉽고 빠르게 할 수 있다는 장점이 있다.

--- src/we/can/coding/Barista.java
+++ src/we/can/coding/Barista.java
@@ -8,6 +8,12 @@ public class Barista {
 	public Barista(String name) {
 		this.name = name;
 		//this의 경우 파라미터 변수와 속성변수가 동일할 경우 사용한다. 이때 this를 사용하지 않으면 생성자 안에 쓰인 name이 모두 지역변수로 간주된다.
+		//파라미터(String n)가 속성인 인스턴스 변수(name)와 다를 경우 name = n;으로 작성이 가능하다.
+	}
+	
+	public Barista(String name, int salary) { //파라미터의 개수가 다를 때
+		this.name = name;
+		this.salary = salary;
 	}
 	
 	public void makeCoffee(OrderSheet order) {
@@ -21,6 +27,6 @@ public class Barista {
 	}
 	
 	public void showInfo() {
-		System.out.println("*** 바리스타 "+name+"가/이 만든 총 커피의 개수는 "+coffeeTotalNum+"입니다.");
+		System.out.println("*** 바리스타 "+name+"가/이 만든 총 커피의 개수는 "+coffeeTotalNum+"이며 연봉은 "+salary+"입니다.");
 	}
 }
--- src/we/can/coding/Main.java
+++ src/we/can/coding/Main.java
@@ -2,7 +2,10 @@ package we.can.coding;
 
 public class Main {
 	public static void main(String[] args) {
-		Barista baristaLee = new Barista("구슬기");
-		baristaLee.showInfo();
+		Barista baristaGu = new Barista("구슬기");
+		baristaGu.showInfo();
+		
+		Barista baristaKim = new Barista("김연경",300000000);
+		baristaKim.showInfo();
 	}
 }
