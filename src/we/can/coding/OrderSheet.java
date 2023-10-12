3ab2f9f9867bb2cbf97a3133099fee15b7b37faa
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-12 23:25:19 +0900

    static 변수 serialNum에 this를 사용하지 않은 이유 : static 변수는 인스턴스 생성과 상관이 없으므로 인스턴스 자기 자신을 의미하는 this 키워드를 쓰지 않는다

--- src/we/can/coding/OrderSheet.java
+++ src/we/can/coding/OrderSheet.java
@@ -9,7 +9,7 @@ public class OrderSheet {
 	
 	public OrderSheet(int coffeeNum) { //커피 종류가 하나라는 가정 하에 주문이 들어온 커피의 개수(coffeeNum)만을 받는다
 		this.coffeeNum = coffeeNum;
-		serialNum++;
+		serialNum++; //static 변수는 인스턴스 생성과 상관이 없으므로 인스턴스 자기 자신을 의미하는 this 키워드를 쓰지 않는다
 		orderNum = serialNum; //orderNum : 주문번호
 		isCompleted = false; //주문이 막 들어왔을 때이므로 주문서의 완료 여부는 false
 	}
