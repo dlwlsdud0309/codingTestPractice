75b56aca598b364a9b7141f73098a75a9d0c817b
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-13 14:53:11 +0900

    인터페이스에 대하여, interface Cleaning 생성 인터페이스의 특징 1. 접근지정자는 public을 사용한다(다른 클래스에서 사용한다는 가정) 2. public abstract이라는 키워드가 붙어야 하나 이는 생략이 가능하다. 3. 비유하자면, '~행동을 할 것이다'의 정도로 메서드를 정의하며 가져다 쓰는 쪽에서 원하는 행동을 정의하는 구조이다. 4. 인스턴스를 생성할 수 없다. 그러므로 생성자가 존재하지 않는다(3에서 설명한 것처럼 뼈대만 줄 뿐이다). 5. 속성은 인스턴스 상관없이 존재한다. 그러므로 static 키워드를 사용한다. 이 경우 public static final 키워드가 붙으나 이 또한 생략이 가능하다. 6. 상속이 extends를 사용하는 것처럼 인터페이스는 implements를 사용한다.

---
+++ src/we/can/coding/Cleaning.java
@@ -0,0 +1,9 @@
+package we.can.coding;
+
+public interface Cleaning {
+	//인터페이스는 몸체가 없는 메서드만 사용할 수 있다.
+	//앞에 public abstract void가 생략되어있다. 아래는 줄임말임.
+	void sweep();
+	void wipe();
+	void arrange();
+}
