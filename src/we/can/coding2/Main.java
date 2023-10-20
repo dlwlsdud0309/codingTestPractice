fa39c1baa64f7b96d7792dbb2c37e8e9f52751bd
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-20 13:42:02 +0900

    파라미터를 받는 생성자 없이 ★기본 생성자만 사용할 경우 객체를 초기화하는 과정이 오래 걸린다(이 경우, 기본 생성자는 자동 생성되기 때문에 코드를 따로 입력하지 않아도 오류는 없다).

--- src/we/can/coding2/Main.java
+++ src/we/can/coding2/Main.java
@@ -6,8 +6,16 @@ public class Main {
 		//new 연산자를 통해 바리스타 인스턴스를 생성한 것
 		//이와 같은 과정을 '객체 초기화'라고 한다.
 		//생성과 동시에 값을 넣어주는 것이다.
-		Barista baristaLee = new Barista("Lee");
+		Barista baristaLee = new Barista("Lee"); //baristaLee는 Barista라는 클래스로 만들어지는 인스턴스이기 때문에 타입이 Barista이다.
 		baristaLee.showInfo();
+		
+		Barista baristaKim = new Barista("Kim", 3000);
+		
+		//파라미터를 받는 생성자 없이 기본 생성자만 사용할 경우
+		Barista baristaKim2 = new Barista();
+		baristaKim2.name = "Kim2";
+		baristaKim2.salary = 5000; //객체를 초기화하는 과정이 오래 걸림
+		
 		
 //		Barista Kim = new Barista(); //기본생성자가 있어야 생성가능
 	}
