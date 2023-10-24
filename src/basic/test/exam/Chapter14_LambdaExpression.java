33740ec018cc2b609beba4ab6b4dda97f1858eaa
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-24 23:29:24 +0900

    람다에 대하여2 인터페이스 계산기 Calc 작성, int로 매개변수 x와 y 지정 메인에서 add와 minus 작성하여 출력, 확인

--- src/basic/test/exam/Chapter14_LambdaExpression.java
+++ src/basic/test/exam/Chapter14_LambdaExpression.java
@@ -1,15 +1,26 @@
 package basic.test.exam;
 
 public class Chapter14_LambdaExpression {
-	int max(int a, int b) {
-		return a>b ? a:b;
+//	int max(int a, int b) {
+//		return a>b ? a:b;
+//	}
+//	
+//	(int a, int b) -> {
+//		return a>b ? a:b; };
+	
+//	(String name, int i) -> 
+//		System.out.println(name+"="+i)
+	
+	interface MyFunction{
+		int calc(int x, int y);
 	}
 	
-	(int a, int b) -> 
-		return a>b ? a:b;
-	
-
 	public static void main(String[] args) {
+		MyFunction add=(x,y)->{
+			return x+y;
+		};
 		
+		System.out.println(add.calc(1, 2));
 	}
+	
 }
