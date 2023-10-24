8fa8470fa27517782e20af0edac127855234d0b8
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-24 23:11:17 +0900

    람다식에 대하여

---
+++ src/basic/test/exam/Chapter14_LambdaExpression.java
@@ -0,0 +1,15 @@
+package basic.test.exam;
+
+public class Chapter14_LambdaExpression {
+	int max(int a, int b) {
+		return a>b ? a:b;
+	}
+	
+	(int a, int b) -> 
+		return a>b ? a:b;
+	
+
+	public static void main(String[] args) {
+		
+	}
+}
