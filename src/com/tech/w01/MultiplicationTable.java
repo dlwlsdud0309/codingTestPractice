2370b8762d95b09ffab84fb414bc1fa4f019df85
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-26 22:45:27 +0900

    for문 사용하여 구구단 출력

---
+++ src/com/tech/w01/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package com.tech.w01;
+
+public class MultiplicationTable {
+	public static void main(String[] args) {
+		for (int i = 2; i <= 9; i++) {
+			System.out.println("2 * "+i+" = "+(2*i));
+//			for (int j = 3; j < 9; j++) {
+//				System.out.print("3 * "+j+" = "+(3*j)+"\t");
+//			}
+		}
+	}
+}
