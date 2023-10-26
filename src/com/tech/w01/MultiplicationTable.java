1eccd51a0884b3a8b1a7928e5338c1dfb11c7fc1
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-26 23:16:14 +0900

    가로 구구단 정렬

--- src/com/tech/w01/MultiplicationTable.java
+++ src/com/tech/w01/MultiplicationTable.java
@@ -3,10 +3,15 @@ package com.tech.w01;
 public class MultiplicationTable {
 	public static void main(String[] args) {
 		for (int i = 2; i <= 9; i++) {
-			System.out.println("2 * "+i+" = "+(2*i));
-//			for (int j = 3; j < 9; j++) {
-//				System.out.print("3 * "+j+" = "+(3*j)+"\t");
-//			}
+//			System.out.println("2 * "+i+" = "+(2*i));
+//			System.out.print(i);
+			for (int j = 2; j <= 9; j++) {
+				System.out.print(i+" * "+j+" = "+(i*j)+"\t");
+			}
+			System.out.println();
 		}
+		
+		
 	}
+	
 }
