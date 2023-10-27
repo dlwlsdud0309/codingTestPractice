fa8d2ea099d75192d7746613ea88bab846264949
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-27 16:54:17 +0900

    구구단을 세로 출력하고 싶은 경우 i와 j의 위치를 바꾸면 된다

--- src/com/tech/w01/MultiplicationTable.java
+++ src/com/tech/w01/MultiplicationTable.java
@@ -2,15 +2,36 @@ package com.tech.w01;
 
 public class MultiplicationTable {
 	public static void main(String[] args) {
+		
 		for (int i = 2; i <= 9; i++) {
-//			System.out.println("2 * "+i+" = "+(2*i));
-//			System.out.print(i);
 			for (int j = 2; j <= 9; j++) {
-				System.out.print(i+" * "+j+" = "+(i*j)+"\t");
+				if ((j*i)<10) {
+					System.out.print(j+" * "+i+" = "+"  "+(j*i));
+				} else {
+					System.out.print(j+" * "+i+" = "+" "+(j*i));
+				}
+				System.out.print("  |  ");
 			}
 			System.out.println();
 		}
 		
+		
+		System.out.println("======");
+		for (int i = 2; i <= 9; i++) {
+//			System.out.print("i = "+i+" ");
+//			System.out.print("2 * "+i+" = "+(2*i)+" ");
+			for (int j = 2; j <= 9; j++) {
+//				System.out.print(i+","+j+"\t");
+//				System.out.print("j = "+j+" ");
+				if((i*j)<10) {
+					System.out.print(i+" * "+j+" =  "+(i*j)+"\t");
+				}else {
+				System.out.print(i+" * "+j+" = "+(i*j)+"\t");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("======");
 		
 	}
 	
