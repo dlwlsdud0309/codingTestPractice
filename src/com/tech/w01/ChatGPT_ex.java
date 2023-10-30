ae9905c5e3ba30ebdafdc39747b6ee5aacc5f2e6
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-30 15:28:44 +0900

    for문 사용하여 숫자 피라미드 만들기1

--- src/com/tech/w01/ChatGPT_ex.java
+++ src/com/tech/w01/ChatGPT_ex.java
@@ -2,6 +2,7 @@ package com.tech.w01;
 
 public class ChatGPT_ex {
 	public static void main(String[] args) {
+		
 		// 구구단 출력하기
 		//세로
 		for (int i = 2; i <= 9; i++) {
@@ -14,6 +15,8 @@ public class ChatGPT_ex {
 		}
 		
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@");
+		
+		//가로
 		for (int i = 2; i <= 9; i++) {
 			for (int j = 2; j < 9; j++) {
 				if(i*j<10) {
@@ -25,5 +28,38 @@ public class ChatGPT_ex {
 			}
 			System.out.println();
 		}
+
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@");
+		
+		//숫자 피라미드
+		//12345
+		// 2345
+		//  345
+		//   45
+		//    5
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = i; j <= 5; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("===================");
+		
+		//1
+		//12
+		//123
+		//1234
+		//12345
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(j+1);
+			}
+			System.out.println();
+		}
+				
 	}
 }
