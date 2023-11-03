e843d34a58081de8d4516059271d57c9be327cf3
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-11-03 15:04:25 +0900

    문자 리터럴인 char는 공백일지라도 하나의 문자가 반드시 필요하나, 문자열 리터럴인 String의 경우는 아무런 문자를 넣지 않아도 오류가 발생하지 않는다. String은 클래스 이므로 연산자 new를 사용해야 하지만 일반 변수 선언하듯 간단한 작성이 가능하다.

--- src/basic/test/exam/TheStandardOfJava.java
+++ src/basic/test/exam/TheStandardOfJava.java
@@ -13,7 +13,14 @@ public class TheStandardOfJava {
 		System.out.println(5%3); //2
 		System.out.println('A');
 		char a = 'A'; //작은따옴표로 문자 하나를 감싼 것 = 문자리터럴
+		char empty1 = ' '; //공백문자 가능 
+//		char empty2 = '';  //에러, 반드시 하나의 문자가 필요
+		String emptyStr1 =" "; //공백문자 가능
+		String emptyStr2 =""; //내용이 없는 빈 문자열 가능
 		String b = "AB"; //두 문자 이상은 큰따옴표로 감싸야 하며 이는 문자열 리터럴 이다.
 		System.out.println(a);
+		
+		String name = new String("Java"); //String 객체를 생성
+		String name2 = "Java";
 	}
 }
