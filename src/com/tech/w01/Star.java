6b42f1d1022c513b73ceeb257efc0cd90e6d576c
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-11-14 14:04:16 +0900

    i가 0일때 j는 최소 0까지 감소하나, j가 후위연산자이므로 0에서 한번 더 감소한 -1까지 출력된다

--- src/com/tech/w01/Star.java
+++ src/com/tech/w01/Star.java
@@ -24,5 +24,44 @@ public class Star {
 			}
 			System.out.println();
 		}
+		
+		System.out.println();
+		
+		//피라미드
+		for (int i = 0; i < 9; i++) { //9줄
+			for (int j = 1; j <= i; j++) { //i는 0에서 8까지 존재, j는 1부터 i가 1일 때 까지 공백, 그러므로 총 8줄
+				System.out.print(" ");
+			}
+			for (int j = 9; j >= i*2-1; j--) { //j는 9에서 홀수인 9,7,5,3,1에 해당하는 i보다 크거나 같을 때까지 감소
+				System.out.print("*"); //11
+			}
+			System.out.println();
+		}
+		
+		//??
+		for (int i = 0; i < 9; i++) {
+			for (int j = 9; j >= i*2-1; j--) {
+				System.out.print(j); // 9에서 0과 -1까지 출력되어 총 11개의 숫자
+			}
+			System.out.println();
+		}
+		
+		//*********
+		// *******
+		//  *****
+		//   ***
+		//    *
+		for (int i = 1; i <= 5; i++) {
+			//공백부터
+			for (int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 9; j >= i*2-1; j--) {
+//					for (int j = 1; j <= 2 * (5 - i) + 1; j++) {
+				System.out.print("*");
+			}
+				
+			System.out.println();
+		}
 	}
 }
