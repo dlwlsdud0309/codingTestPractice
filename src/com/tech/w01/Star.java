e1670a3a5b059307266fe396cf1e79c33a9d6eac
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-11-17 14:48:54 +0900

    for문 사용하여 모래시계 만들기

--- src/com/tech/w01/Star.java
+++ src/com/tech/w01/Star.java
@@ -59,24 +59,22 @@ public class Star {
 			}
 			for (int j = 9; j >= i*2-1; j--) {
 //					for (int j = 1; j <= 2 * (5 - i) + 1; j++) {
-				System.out.print("!");
+				System.out.print("*");
 			}
 				
 			System.out.println();
 		}
-		
-		System.out.println();
-		
 		//피라미드
-		for (int i = 1; i <= 5; i++) {
+		for (int i = 2; i <= 5; i++) {
 			for (int j = 5; j > i; j--) {
 				System.out.print(" ");
 			}
 			for (int j = 0; j < i*2-1; j++) {
 				System.out.print("*");
-				if(j==5 && i*2-1==9) { //i가 5일때 j는 
-					System.out.print(i);
-				}
+//				System.out.print(i+","+j+" ");
+//				if(j==5 && i*2-1==9) { //i가 5일때 j는 
+//					System.out.print(i+","+j);
+//				}
 			}
 			System.out.println();
 		}
