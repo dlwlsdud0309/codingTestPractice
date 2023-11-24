86bff66683c744c75caa89e54e9e2480274007da
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-11-24 14:06:21 +0900

    피라미드 출력, 코드 비교하기

--- src/com/tech/w01/MultiplicationTableAndStar.java
+++ src/com/tech/w01/MultiplicationTableAndStar.java
@@ -133,6 +133,37 @@ public class MultiplicationTableAndStar {
 			System.out.println();
 		}
 		
+//		**********
+//		 ********
+//		  ******
+//		   ****
+//		    **
+		System.out.println("====");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 9; j > i*2-1; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		//*********
+		// *******
+		//  *****
+		//   ***
+		//    *
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 9; j >= i*2-1; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
 		
 //		for (int i = 0; i < 5; i++) {
 ////			System.out.print("@");
